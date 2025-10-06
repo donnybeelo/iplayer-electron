663d6b3ecb1f0ca49a6ee34298bc97bb3745534e
Daniel Elia <d.elia@outlook.com>
2025-10-06 17:07:54 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const electron = require('electron');
+const path = require('path')
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+const fullscreen = process.argv.includes('--fullscreen') || process.argv.includes('-f');
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    backgroundColor: 'black',
+    width: 1280,
+    height: 720,
+    fullscreen,
+    titleBarStyle: "hidden",
+    ...(process.platform !== 'darwin' ? { titleBarOverlay: { color: '#000000', symbolColor: '#ffffff', height: '15px' } } : {})
+  });
+
+  mainWindow.setTitle('BBC iPlayer');
+  mainWindow.loadURL('http://bbc.co.uk/iplayer');
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+  mainWindow.on('page-title-updated', () => {
+    mainWindow.webContents.insertCSS(`
+      ::-webkit-scrollbar { width: 0px; }
+      #orbit-header { app-region: drag; position: sticky; top: 0; z-index: 2000; }
+      .orbit-header-links { display: none !important; }
+      .orbit-header-right { margin-left: 0 !important; }
+      .orb-nav-pri-container { justify-content: normal !important; }
+      .orb-nav-pri-container > * { app-region: no-drag; }
+    `);
+  })
+});
